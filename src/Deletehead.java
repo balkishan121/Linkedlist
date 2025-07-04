bbd95f01b8650fe4efbcf2f43ae2e3b0d403666b
balki <balkishanpadihar@gmail.com>
2025-07-04 16:37:41 +0530

    head deleted in singly linked list

--- src/Deletehead.java
+++ src/Deletehead.java
@@ -7,12 +7,15 @@ public class Deletehead {
     }
 
     public static void main(String[] args) {
-        int[] arr={2,5,7,8,9,45,14,22,1,7,1,9,78,3,574,9};
+        int[] arr={2,5,7,8,9,45,14,22,19};
         Node head = Main.ArrtoLl(arr);
         System.out.print("Given Linked List is: ");
         Main.printLl(head);
         System.out.println("\nHead of the Linked List is: "+head.data);
         head=deletehead(head);
-        System.out.println("Head after Removal old head is: "+head.data);
+        System.out.print("Modified Linked List is: ");
+        Main.printLl(head);
+        System.out.println("\nHead after Removal old head is: "+head.data);
+
     }
 }
